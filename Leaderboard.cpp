2ee0dbb5dfca143dee968bc311161e1346a843c8
Renardenboite <foxin1917@gmail.com>
2026-05-12 20:12:04 +0300

    Add files via upload

---
+++ Leaderboard.cpp
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <algorithm>
+#include "Leaderboard.h"
+#include "Game.h"
+
+namespace ApplesGame
+{
+    void AddItemToLeaderboard(Game& game)
+    {
+        const auto playerName = game.UI.askNicknameMenu.nicknameInput.isEmpty() ? L"XYZ" : game.UI.askNicknameMenu.nicknameInput.toWideString();
+        
+        auto& leaderboard = game.leaderboard.array;
+        leaderboard.push_back({ playerName, game.score });
+
+        std::stable_sort(leaderboard.begin(), leaderboard.end(), [](const LeaderboardItem& item1, const LeaderboardItem& item2)
+            {
+                return item1.score > item2.score;
+            });
+
+        if (leaderboard.size() > 10)
+        {
+            leaderboard.resize(10);
+        }
+    }
+
+    std::vector<LeaderboardItem> GetSortedLeaderboard(std::vector<LeaderboardItem> leaderboard)
+    {
+        std::stable_sort(leaderboard.begin(), leaderboard.end(), [](const LeaderboardItem& item1, const LeaderboardItem& item2)
+            {
+                return item1.score > item2.score;
+            });
+
+        return leaderboard;
+    }
+
+    bool SerializeAndSaveGame(const Leaderboard& leaderboard)
+    {
+        std::wofstream file(LEADERBOARD_FILE_PATH);
+
+        if (file.is_open())
+        {
+            for (auto item : leaderboard.array)
+            {
+                std::replace(item.playerName.begin(), item.playerName.end(), L' ', L'_');
+
+                file << item.playerName << L" " << item.score << "\n";
+            }
+
+            file.close();
+            return true;
+        }
+
+        return false;
+    }
+
+    bool DeserializeAndLoadLeaderboard(Leaderboard& leaderboard)
+    {
+        std::wifstream file(LEADERBOARD_FILE_PATH);
+
+        if (file.is_open())
+        {
+            leaderboard.array.clear();
+            LeaderboardItem tmpItem;
+
+            while (file >> tmpItem.playerName >> tmpItem.score)
+            {
+                std::replace(tmpItem.playerName.begin(), tmpItem.playerName.end(), L'_', L' ');
+                leaderboard.array.push_back(tmpItem);
+            }
+
+            file.close();
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ClearLeaderboard(Game& game)
+    {
+        game.leaderboard.array.clear();
+        game.UI.leaderboardMenu.leaderboard.clear();
+
+        std::wofstream file(LEADERBOARD_FILE_PATH, std::ios::out | std::ios::trunc);
+
+        if (file.is_open())
+        {
+            file.close();
+            return true;
+        }
+
+        return false;
+    }
+}
